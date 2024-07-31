b7c1bfe0bee3c4182a3775aeac2acd3f5a7530c9
erwin <yohey03518@gmail.com>
2024-07-31 10:30:24 +0800

    init UserApi project

---
+++ app/UserApi/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel((_, options) =>
+{
+    options.Listen(IPAddress.Any,  53664, listenOptions => { listenOptions.Protocols = HttpProtocols.Http1; });
+});
+
+var app = builder.Build();
+
+app.Map("/ping", httpContext => httpContext.Response.WriteAsync("pong"));
+app.Run();
