53d4540a17248dd1cd05fd44651dcc95545a7a22
erwin <yohey03518@gmail.com>
2024-08-18 13:52:59 +0800

    clean up

--- app/LoggingSdk/AccessLogMiddleware.cs
+++ app/LoggingSdk/AccessLogMiddleware.cs
@@ -1,17 +1,14 @@
-﻿using System.Text;
-
-namespace LoggingSdk;
+﻿namespace LoggingSdk;
 
 public class AccessLogMiddleware(RequestDelegate next, ILogger<AccessLogMiddleware> logger)
 {
-
     public async Task Invoke(HttpContext context)
     {
         var request = context.Request;
         request.EnableBuffering();
         var requestBodyText = await new StreamReader(request.Body).ReadToEndAsync();
         request.Body.Position = 0;
-        
+
         logger.LogInformation("hello middleware {requestBody}", requestBodyText);
         await next(context);
     }
