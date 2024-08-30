c1b2a3b93a00bad7cef012f70f0eb7d75dbb82b7
erwin <yohey03518@gmail.com>
2024-08-31 00:50:23 +0800

    adjust log to uppercase

--- app/LoggingSdk/GrpcLoggingInterceptor.cs
+++ app/LoggingSdk/GrpcLoggingInterceptor.cs
@@ -12,25 +12,17 @@ public class GrpcLoggingInterceptor(ILogger<GrpcLoggingInterceptor> logger) : In
         ClientInterceptorContext<TRequest, TResponse> context,
         AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
     {
-        try
-        {
-            logger.LogInformation("send gRPC Request Start [{method}] [{Request}]", context.Method.FullName, request.ToString());
-            
-            var stopwatch = Stopwatch.StartNew();
-            var call = continuation(request, context);
+        logger.LogInformation("Send gRPC Request Start [{method}] [{Request}]", context.Method.FullName, request.ToString());
 
-            return new AsyncUnaryCall<TResponse>(
-                LogClientResponse(context.Method.FullName, call, stopwatch),
-                call.ResponseHeadersAsync,
-                call.GetStatus,
-                call.GetTrailers,
-                call.Dispose);
-        }
-        catch (RpcException e)
-        {
-            logger.LogWarning(e, "Grpc Error");
-            throw;
-        }
+        var stopwatch = Stopwatch.StartNew();
+        var call = continuation(request, context);
+
+        return new AsyncUnaryCall<TResponse>(
+            LogClientResponse(context.Method.FullName, call, stopwatch),
+            call.ResponseHeadersAsync,
+            call.GetStatus,
+            call.GetTrailers,
+            call.Dispose);
     }
 
     // server logging
@@ -39,27 +31,19 @@ public class GrpcLoggingInterceptor(ILogger<GrpcLoggingInterceptor> logger) : In
         ServerCallContext context,
         UnaryServerMethod<TRequest, TResponse> continuation)
     {
-        try
-        {
-            logger.LogInformation("receive gRPC Request Start [{host}] [{method}] [{Request}]", context.Host, context.Method, request.ToString());
-            var stopwatch = Stopwatch.StartNew();
-            var response = await continuation(request, context);
-            stopwatch.Stop();
-            var milliseconds = stopwatch.ElapsedMilliseconds;
-            logger.LogInformation("receive gRPC Request End [{host}] [{method}] [{time}ms] [{Response}]", context.Host, context.Method, milliseconds, response.ToString());
-            return response;
-        }
-        catch (RpcException e)
-        {
-            logger.LogWarning(e, "Grpc Error");
-            throw;
-        }
+        logger.LogInformation("Receive gRPC Request Start [{host}] [{method}] [{Request}]", context.Host, context.Method, request.ToString());
+        var stopwatch = Stopwatch.StartNew();
+        var response = await continuation(request, context);
+        stopwatch.Stop();
+        var milliseconds = stopwatch.ElapsedMilliseconds;
+        logger.LogInformation("Receive gRPC Request End [{host}] [{method}] [{time}ms] [{Response}]", context.Host, context.Method, milliseconds, response.ToString());
+        return response;
     }
 
     private async Task<TResponse> LogClientResponse<TResponse>(string fullName, AsyncUnaryCall<TResponse> call, Stopwatch stopwatch)
     {
         var response = await call;
-        logger.LogInformation("send gRPC Request End [{method}] [{time}ms] [{Response}]", fullName, stopwatch.ElapsedMilliseconds, response?.ToString() ?? string.Empty);
+        logger.LogInformation("Send gRPC Request End [{method}] [{time}ms] [{Response}]", fullName, stopwatch.ElapsedMilliseconds, response?.ToString() ?? string.Empty);
         return response;
     }
 }
